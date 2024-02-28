d72c7ee91064b7a22eb01e1d96991157488c322e
nikl216 <nikhilnalin216@gmail.com>
2024-02-28 16:23:00 +0530

    Update workflow node version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/NooraHealth/LLM-Project-doc-site/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/NooraHealth/LLM-Project-doc-site/tree/main/",
         },
         theme: {
           customCss: "./src/css/custom.css",
